eaa96d5c9625fef1bb1c4c576773589b19354759
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-29 17:03:33 -0600

    feat: implement Product class with code, name, and price attributes

---
+++ src/main/java/com/alfonsovidrio/invoiceapp/models/Product.java
@@ -0,0 +1,39 @@
+package com.alfonsovidrio.invoiceapp.models;
+
+public class Product {
+    private int code;
+    private String name;
+    private float price;
+    private static int lastCode;
+
+    public Product() {
+        this.code = ++lastCode;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return code +
+                "\t'" + name +
+                "\t" + price;
+    }
+}
