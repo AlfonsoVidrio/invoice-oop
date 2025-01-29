cc53ea249ee973a8c7b152376ccd89291bac485d
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-01-29 17:04:37 -0600

    feat: create Customer class to store customer information

---
+++ src/main/java/com/alfonsovidrio/invoiceapp/models/Customer.java
@@ -0,0 +1,22 @@
+package com.alfonsovidrio.invoiceapp.models;
+
+public class Customer {
+    private String name;
+    private String rfc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+}
